a34b7626a1ca969cc60763c2a1ce5a674ed3ab49
alt-qi <a-ti-radeon@yandex.ru>
2022-05-27 12:15:32 +0700

    added some snackbars to settings screen

--- app/src/main/java/ru/altqi/exp/ui/PreferencesScreenFragment.java
+++ app/src/main/java/ru/altqi/exp/ui/PreferencesScreenFragment.java
@@ -1,38 +1,43 @@
 package ru.altqi.exp.ui;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.google.android.material.snackbar.Snackbar;
+
 import ru.altqi.exp.R;
 import ru.altqi.exp.data.FormulaDatabase;
 
 public class PreferencesScreenFragment extends PreferenceFragmentCompat {
 
+    Preference clearDBButton, clearFavorites;
+    FormulaDatabase db;
+
     @Override
     public void onCreatePreferences(@Nullable Bundle savedInstanceState, @Nullable String rootKey) {
         setPreferencesFromResource(R.xml.preferences, rootKey);
-        FormulaDatabase db = FormulaDatabase.getDatabase(getLayoutInflater().getContext());
-        Preference clearDBButton = findPreference("clear_db");
-        Preference clearFavorites = findPreference("clear_favorites");
+        db = FormulaDatabase.getDatabase(getLayoutInflater().getContext());
+        clearDBButton = findPreference("clear_db");
+        clearFavorites = findPreference("clear_favorites");
 
-        clearDBButton.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                db.formulaDao().deleteAllFormulas();
-                return true;
-            }
+        clearDBButton.setOnPreferenceClickListener(preference -> {
+            db.formulaDao().deleteAllFormulas();
+            Snackbar.make(getActivity().findViewById(R.id.nav_host_fragment),
+                    "Все формулы удалены.", Snackbar.LENGTH_SHORT).show();
+            return true;
         });
 
-        clearFavorites.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
-            @Override
-            public boolean onPreferenceClick(@NonNull Preference preference) {
-                db.formulaDao().deleteAllFromFavorites();
-                return true;
-            }
+        clearFavorites.setOnPreferenceClickListener(preference -> {
+            db.formulaDao().deleteAllFromFavorites();
+            Snackbar.make(getActivity().findViewById(R.id.nav_host_fragment),
+                    "Избранное очищено.", Snackbar.LENGTH_SHORT).show();
+            return true;
         });
     }
 }
